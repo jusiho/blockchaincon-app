9f60706dd6b73f3f66a097945ddada3f8d8fde57
jusiho <pierreblas123@gmail.com>
2024-06-22 18:47:48 -0500

    feat: Add bulk approval functionality to TableConfirm component

--- src/app/admin/TableConfirm.tsx
+++ src/app/admin/TableConfirm.tsx
@@ -133,6 +133,7 @@ export default function TableWith() {
     direction: "ascending",
   });
   const [page, setPage] = useState(1);
+  const [checked, setChecked] = React.useState<string[]>([]);
 
   const {
     data: users,
@@ -146,7 +147,6 @@ export default function TableWith() {
     }
   );
   console.log(users);
-  
 
   const pages = users?.total_pages;
 
@@ -405,7 +405,7 @@ export default function TableWith() {
             onValueChange={onSearchChange}
           />
         </div>
-        <div>
+        <div className="flex gap-4">
           <button
             className="bg-primary text-white rounded-md px-4 py-2"
             onClick={() => signOut()}
@@ -457,8 +457,57 @@ export default function TableWith() {
   const loadingState =
     isLoading || users?.total_records === 0 ? "loading" : "idle";
 
+  const handleMasivo = async () => {
+    console.log("Aprobar masivo :", checked);
+    setLoading(true);
+    if (checked.length > 0) {
+      try {
+        await Promise.all(
+          checked.map(async (user) => {
+            try {
+              const response = await fetch(
+                `${process.env.NEXT_PUBLIC_URL_LOCAL}/api/updateRegister`,
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    id_register: user,
+                    state: "1",
+                  }),
+                }
+              );
+              console.log("response : ", response);
+
+              // Aquí podrías manejar la respuesta, como verificar el estado HTTP o parsear JSON.
+            } catch (error) {
+              console.error("Error al actualizar el registro:", user, error);
+              // Manejar cada error individualmente
+            }
+          })
+        );
+      } catch (error) {
+        // Code to handle error
+      } finally {
+        // Deseleccionar
+        setSelectedKeys(new Set([]));
+        setChecked([]);
+
+        setLoading(false);
+        mutate();
+      }
+    }
+  };
+
   return (
     <div>
+      <button
+        className="bg-primary text-white rounded-md px-4 py-2"
+        onClick={handleMasivo}
+      >
+        Aprobar masivo
+      </button>
       <Table
         aria-label="Example table with custom cells, pagination and sorting"
         isHeaderSticky
